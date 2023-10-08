af3e8b40ed275f6d7868c0ade4e9b18d1615b4b7
Ammar moulla <ammarturing@gmail.com>
2023-10-08 14:33:34 +0300

    fix migrate bug

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 script_path=$(readlink -f "$0")
 script_dir=$(dirname "$script_path")
+python manage.py migrate
 python ${script_dir}/server.py 
--- video/helper.py
+++ video/helper.py
@@ -14,7 +14,7 @@ def edit_video(video_path, text_clip, x, y, timestep, duration, fontsize):
     video_text_path = new_path(video_path)
     video = VideoFileClip(video_path)
     
-    text_clip = TextClip(text_clip, fontsize=int(fontsize, color='white')).set_position((x, y)).set_duration(duration)
+    text_clip = TextClip(text_clip, fontsize=int(fontsize), color='white').set_position((x, y)).set_duration(duration)
             
     video_with_text = video.set_duration(duration).set_audio(None).add_textclip(text_clip)
 
