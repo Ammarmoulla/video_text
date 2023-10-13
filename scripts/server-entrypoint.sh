6a40385dffe8eeee19f11b438db9f2e34dbd5c91
Ammar moulla <ammarturing@gmail.com>
2023-10-13 21:27:51 +0300

    add ngrok images

--- scripts/server-entrypoint.sh
+++ scripts/server-entrypoint.sh
@@ -2,10 +2,10 @@
 script_path=$(readlink -f "$0")
 script_dir=$(dirname "$script_path")
 
-#ngrok auth and run
-token=$(sed -n "s/ngrok_token=\(.*\)/\1/p" < ${PWD}/.env)
-ngrok config add-authtoken $token
-ngrok http 8000
+# #ngrok auth and run
+# token=$(sed -n "s/ngrok_token=\(.*\)/\1/p" < ${PWD}/.env)
+# ngrok config add-authtoken $token
+# ngrok http 8000 &
 
 
 until python manage.py migrate
@@ -20,7 +20,6 @@ done
 # python manage.py createsuperuser --noinput
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin_git', 'admin@example.com', 'admingit')"
 
-# gunicorn  --bind 0.0.0.0:8000 --workers 5 --threads 3
 
 gunicorn core.wsgi --worker-class=gevent --worker-connections=1000 --workers=5
 
